dcb3bb43eb9d281766e0aa0fcd1628faa8832ac0
sthangav <your_email_address@example.com>
2022-04-11 12:45:45 +0530

    Added SSSSSSSSS

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -5,6 +5,7 @@ import azure.functions as func
 
 def main(myblob: func.InputStream):
     print("My new blob trigger")
+    logging.info("SSSSSSSSSSSSSSSSSSSS")
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
